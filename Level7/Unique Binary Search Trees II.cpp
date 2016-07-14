084c07d4414b3b3d8f5dfe3749eb90b42eaa1021
Diego González Hernández <briares2698@gmail.com>
2016-07-14 02:50:35 -0500

    D

---
+++ Level7/Unique Binary Search Trees II.cpp
@@ -0,0 +1,14 @@
+int memo[1002];
+int dp(int x){
+	if(x==1)return 1;
+	if(memo[x])return memo[x];
+	int s=0;
+	for(int i=1;i<x;i++){
+		s+=((dp(i))*dp(x-i));
+	}
+	return memo[x]=s;
+}
+int Solution::numTrees(int A) {
+   memset(memo,0,sizeof(memo));
+   return dp(A+1);
+}
